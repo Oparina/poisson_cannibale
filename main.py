734412938130168c039f4e28bdedb598cb7e1a7c
lut <lut@educ.college-montreal.qc.ca>
2022-05-05 15:38:50 -0400

    test

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ from enemy_fish import EnemyFish
 import game_constants as gc
 from game_state import GameState
 
-#tengze reporting in
-
 class MyGame(arcade.Window):
     """
     La classe principale de l'application
